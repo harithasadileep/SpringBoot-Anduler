caa0dc442ef7c5e9ed3bf71aff4fe267912139cd
dsomajoh <dsomajoh@DIN66008064.corp.capgemini.com>
2017-03-14 21:00:43 +0530

    Anguler

---
+++ springboot-angularjs/src/main/resources/static/js/app.js
@@ -0,0 +1,27 @@
+var app = angular.module('app', ['ngRoute','ngResource']);
+app.config(function($routeProvider){
+    $routeProvider
+        .when('/users',{
+            templateUrl: '/views/users.html',
+            controller: 'usersController'
+        })
+        .when('/roles',{
+            templateUrl: '/views/roles.html',
+            controller: 'rolesController'
+        })
+        .otherwise(
+            { redirectTo: '/'}
+        );
+});
+
+
+
+app.controller('usersController', function($scope) {
+    $scope.headingTitle = "User List";
+});
+
+app.controller('rolesController', function($scope) {
+    $scope.headingTitle = "Roles List";
+});
+
+
